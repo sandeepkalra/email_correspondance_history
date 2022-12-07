398f2038946d6e6ee4e8f9391f31db85ea8e5f4b
sandeep <sandeep.kalra@gmail.com>
2022-12-07 15:06:22 -0600

    parsed and converted all .eml to text for search purposes

---
+++ parse.py
@@ -0,0 +1,15 @@
+import os
+import email
+from email import policy
+from email.parser import BytesParser
+import glob
+
+file_list = glob.glob('*.eml') # returns list of files
+for file in file_list:
+    with open(file, 'rb') as fp:
+        msg = BytesParser(policy=policy.default).parse(fp)
+        fnm = os.path.splitext(file)[0] + '.txt'
+        txt = msg.get_body(preferencelist=('plain')).get_content()
+        with open(fnm, 'w') as f:
+            print('Filename:', txt, file = f)
+
