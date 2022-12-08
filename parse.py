e3565ec4db51f9ae61de2a888eb65eb0d8aabb85
sandeep <sandeep.kalra@gmail.com>
2022-12-08 14:19:15 -0600

    vim cscope builds are now working

--- parse.py
+++ parse.py
@@ -9,6 +9,7 @@ for file in file_list:
     with open(file, 'rb') as fp:
         msg = BytesParser(policy=policy.default).parse(fp)
         fnm = os.path.splitext(file)[0] + '.txt'
+        fnm = fnm.replace(" ", "_")
         txt = msg.get_body(preferencelist=('plain')).get_content()
         with open(fnm, 'w') as f:
             print('Filename:', txt, file = f, end="")
--- setup.searchable.sh
+++ setup.searchable.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 DIRS=("BCCP"     "CP"       "CPPD"     "EPA"      "FWS.GOV"  "HOA"      "HoaLAWYER" "TBPE" "TXDOT"   "WILCO")
-for dir in $DIRS; do
+rm -rf ./_as_text_
+for dir in ${DIRS[@]};
+do
     cd _with_/$dir
     exec $(python3 ../../parse.py)
     cd ../../
     mkdir -p _as_text_/$dir/
     mv _with_/$dir/*.txt _as_text_/$dir/
+
+    rm cscope.* tags || true; find . -type f -name \*.txt -exec echo \"{}\" \; > cscope.files
+    cat cscope.files | xargs cscope -b
 done
