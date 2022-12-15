1707660832995ff6b57f2e25a8917f49d3aec645
sandeep <sandeep.kalra@gmail.com>
2022-12-14 22:49:21 -0600

    modified parser.py
    The filename of source .eml wasn't printing properly.

--- parse.py
+++ parse.py
@@ -4,13 +4,16 @@ from email import policy
 from email.parser import BytesParser
 import glob
 
+pwd_full=os.environ["PWD"]
+pwd=pwd_full.split("_with_")[1]
 file_list = glob.glob('*.eml') # returns list of files
 for file in file_list:
     with open(file, 'rb') as fp:
         msg = BytesParser(policy=policy.default).parse(fp)
         fnm = os.path.splitext(file)[0] + '.txt'
+        oldFnm = "[\""+pwd+"/"+file+"\"]\r\n"
         fnm = fnm.replace(" ", "_")
-        txt = msg.get_body(preferencelist=('plain')).get_content()
+        content = msg.get_body(preferencelist=('plain')).get_content()
         with open(fnm, 'w') as f:
-            print('Filename:', txt, file = f, end="")
+            print('Filename:', oldFnm, content, file = f, end="")
 
